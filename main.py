4c775905416ea93769ff25f60c1b63ed7cfee1c6
vidushan <vidushanpathir505@gmail.com>
2026-06-27 22:37:19 +0530

    Update expense_manager.py

--- main.py
+++ main.py
@@ -5,27 +5,39 @@ from models.expense import Expense
 from services.expense_manager import ExpenseManager
 #from services.debt_calculator import DebtCalculator
 
+exp = ExpenseManager()
+
 kasun = User("Kasun")
 amara = User("Amara")
 nayana = User("Nayana")
 
 expense1 = Expense(kasun, 1000, "Dinner", [kasun, amara, nayana])
 
-bodima = Group("Bodima")
+bodima = Group("bodima")
 bodima.add_members(kasun)
 bodima.add_members(amara)
 bodima.add_members(nayana)
 bodima.add_expense(expense=expense1)
 
 expense2 = Expense(amara, 2000, "Bus Money", [kasun, amara, nayana])
+
+expense3 = Expense(nayana, 1500, "groccery shop", [kasun, amara, nayana])
+
+expense4 = Expense(amara, 400, "Party", [kasun, amara, nayana])
 bodima.add_expense(expense=expense2)
 
-exp = ExpenseManager()
+exp.show_expenses(bodima)
+
+exp.add_expense(bodima, expense=expense3)
+
+exp.add_expense(bodima, expense=expense4)
+
+#exp.remove_expense(bodima, expense2)
+
+exp.show_members(bodima)
 
 exp.show_expenses(bodima)
 
-print(bodima.expenses)
+print(exp.total_expense(bodima))
 
-exp.remove_expense(bodima, expense2)
-
-exp.show_expenses(bodima)
+exp.show_user_expense(amara, bodima)
--- services/expense_manager.py
+++ services/expense_manager.py
@@ -1,13 +1,13 @@
-from models.expense import Expense
-from models.group import Group
-
-
 class ExpenseManager:
     
     def __init__(self):
         pass
     
     def show_expenses(self, group):
+        
+        if not group.expenses:
+            print("No expenses available")
+            return
         
         for expense in group.expenses:
             print(f"{expense.payer.name} paid Rs.{expense.amount} for {expense.description}")
@@ -20,3 +20,82 @@ class ExpenseManager:
         
         else:
             print("Expense is not Found")
+            
+    def add_expense(self, group, expense):
+        
+        if expense.amount <= 0:
+            print("Invalid amount")
+            return
+        
+        if expense.payer not in group.members:
+            print("Payer not in group")
+            return
+        
+        group.expenses.append(expense)
+        
+        print("Expense add Successful")
+        
+    def show_members(self, group):
+        
+        if not group.members:
+            print("No Members in Group")
+            return
+        
+        for member in group.members:
+            print(member.name)
+        
+    def add_member(self, group, user):
+        
+        if user not in group.members:
+            group.members.append(user)
+            print("Member added")
+            
+    def remove_member(self, group, user):
+        
+        if user in group.members:
+            group.members.remove(user)
+            print("Member Remove")
+            
+    def find_expense(self, group, description):
+        
+        return next((e for e in group.expenses if e.description == description), None)
+    
+    def total_expense(self, group):
+        
+        total = 0
+        
+        for expense in group.expenses:
+            total += expense.amount
+            
+        return total
+    
+    def show_user_expense(self, user, group):
+        
+        if not group.expenses:
+            print("No expenses available")
+            return
+        
+        total = 0
+        
+        for expense in group.expenses:
+            if expense.payer == user:
+                total += expense.amount
+                print(f"{expense.description} | Rs.{expense.amount}")
+        
+        print(f"Total Expenses | {total}")
+        
+        
+    
+    
+        
+        
+        
+        
+    
+    
+        
+        
+    
+    
+    
+        
